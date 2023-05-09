3c6aabe2a64ab6c126d48e50959a6abdce66fed6
Kostya <koooooostaaa@gmail.com>
2023-05-09 20:28:42 +0300

    create new components

---
+++ resources/js/Components/ProductCard.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+import type {IProductCard} from "@/types/IProductCard";
+
+const {product} = defineProps<{product: IProductCard}>()
+</script>
+
+<template>
+    <div class="rounded-xl bg-white relative">
+        <div v-if="product.status" class="absolute py-[7px] px-[18px] text-sm bg-red-bg text-white uppercase left-0 top-5 rounded-r-md">{{ product.status }}</div>
+        <div class="max:h-300 max:w-300 overflow-hidden">
+            <img :alt="product.title" :src="product.thumbnail">
+        </div>
+        <div class="m-5 mt-4">
+            <h2 class="font-semibold text-lg">{{ product.title }}</h2>
+            <p class="mt-3  line-clamp-2">{{  product.description }}</p>
+            <div class="flex justify-between items-center mt-4">
+                <button class="text-white bg-primary py-[13px] px-8 rounded-md">Выбрать</button>
+                <span class="text-primary font-semibold text-lg">от {{ product.price }}$</span>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Components/ProductsGrid.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+
+import ProductCard from "@/Components/ProductCard.vue";
+import FilterIcon from "@/Components/Icons/FilterIcon.vue";
+</script>
+
+<template>
+    <div class="bg-gray-350">
+        <div class="sm:container sm:mx-auto mx-4">
+            <div class="flex justify-between mb-8 items-center">
+                <h3 class="text-[40px] leading-[48px] font-semibold">Пицца</h3>
+                <div class="py-[9px] px-4 bg-white flex items-center rounded-md" role="button">
+                    <filter-icon class="mr-2 h-4 w-4"/>
+                    Фильтры
+                </div>
+            </div>
+
+            <div class="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[30px]">
+                <ProductCard v-for="i in 9" :product="{
+                    id: 1,
+                    title: 'Чикен Сладкий Чили',
+                    description: 'Курица, Лук, Перец Халапеньо, Сыр Моцарелла, Томатный соус, Паприка, Майонез',
+                    price: 4.5,
+                    thumbnail: '/images/nice-pizza.png',
+                    status: 'Hot'}"/>
+            </div>
+        </div>
+    </div>
+</template>
