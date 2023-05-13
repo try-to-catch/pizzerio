9e6f5ea6a3bd2014e06e70639f855d842e1d54db
Kostya <koooooostaaa@gmail.com>
2023-05-13 21:40:23 +0300

    update route name

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 
 Route::get('/pizza', function () {
     return Inertia::render('Products/Index');
-});
+})->name('products');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
