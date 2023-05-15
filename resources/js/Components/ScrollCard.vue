bb792f7f650136546bf3aad4f689623298ef4c29
Kostya <koooooostaaa@gmail.com>
2023-05-15 18:27:08 +0300

    create new component

---
+++ resources/js/Components/ScrollCard.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+const {image, caption} = defineProps<{
+    image: string,
+    caption: string
+}>()
+</script>
+
+
+<template>
+    <div :style="{ backgroundImage: `url(${image})` }"
+         class="rounded-xl bg-cover bg-no-repeat bg-center  relative">
+        <span class="absolute text-xl font-semibold bottom-5 left-5 lg:right-10 right-5 text-white">{{ caption }}</span>
+    </div>
+</template>
