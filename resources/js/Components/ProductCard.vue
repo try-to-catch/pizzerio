12ae4337c269c4e0c041ac40b56b3dcd4ab8707b
Kostya <koooooostaaa@gmail.com>
2023-05-15 18:27:48 +0300

    add border for card

--- resources/js/Components/ProductCard.vue
+++ resources/js/Components/ProductCard.vue
@@ -6,7 +6,7 @@ const {product} = defineProps<{product: IProductCard}>()
 </script>
 
 <template>
-    <div class="rounded-xl bg-white relative">
+    <div class="rounded-xl bg-white relative border border-gray-100">
         <div v-if="product.status" class="absolute py-[7px] px-[18px] text-sm bg-red-bg text-white uppercase left-0 top-5 rounded-r-md">{{ product.status }}</div>
         <div class="max:h-300 max:w-300 overflow-hidden flex items-center justify-center">
             <img :alt="product.title" :src="product.thumbnail">
