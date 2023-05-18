a69c68ba6fa44fa6c26e9948b46bd76cbc1e52c7
Kostya <koooooostaaa@gmail.com>
2023-05-18 20:31:31 +0300

    create new requests

---
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->user()->is_admin = true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'min:3', 'unique:categories'],
+            'icon' => ['required', 'image'],
+        ];
+    }
+}
