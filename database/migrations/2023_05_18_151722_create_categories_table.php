7ef110c167dc00a13122b04871e04be7bfbd3029
Kostya <koooooostaaa@gmail.com>
2023-05-18 20:29:59 +0300

    create new migrations

---
+++ database/migrations/2023_05_18_151722_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('icon');
+
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (app()->isLocal()) {
+            Schema::dropIfExists('categories');
+        }
+    }
+};
