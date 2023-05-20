b87ebbc5677451aa67925916b834770e92163b62
Kostya <koooooostaaa@gmail.com>
2023-05-20 15:43:42 +0300

    remove cascadeOnDelete

--- database/migrations/2023_05_18_151722_create_categories_table.php
+++ database/migrations/2023_05_18_151722_create_categories_table.php
@@ -19,8 +19,7 @@ return new class extends Migration
 
             $table->foreignIdFor(User::class)
                 ->constrained()
-                ->cascadeOnUpdate()
-                ->cascadeOnDelete();
+                ->cascadeOnUpdate();
 
             $table->timestamps();
         });
