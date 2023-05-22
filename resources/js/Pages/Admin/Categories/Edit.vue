6b50ed314c90f971a7e297823c09f050bbcb64fa
Kostya <koooooostaaa@gmail.com>
2023-05-22 16:06:16 +0300

    update component (improve error handle)

--- resources/js/Pages/Admin/Categories/Edit.vue
+++ resources/js/Pages/Admin/Categories/Edit.vue
@@ -1,44 +1,44 @@
 <script lang="ts" setup>
 
 import AdminLayout from "@/Layouts/AdminLayout.vue";
-import {Head, Link, router} from "@inertiajs/vue3";
+import {Head, Link, useForm} from "@inertiajs/vue3";
 import {useField} from "vee-validate"
 import {string} from 'yup'
 import {ICategory} from "@/types/ICategory";
-import {reactive} from "vue";
 
 const {category} = defineProps<{ category: ICategory }>()
 
 const {
     errorMessage: titleError,
     value: titleValue,
-    setErrors: setTitleErrors
 } = useField<string>('title', string().min(3))
 
 titleValue.value = category.title;
 
 const {errorMessage: iconError, value: iconValue} = useField<null | File>('icon')
 
-interface INewCategory {
+interface IUpdatedCategory {
     title: string,
-    icon: null | File
+    icon: null | File,
+    _method: 'put' | 'patch'
 }
 
-const form = reactive<INewCategory>({
+const form = useForm<IUpdatedCategory>({
     title: '',
     icon: null,
+    _method: 'patch'
 })
 
-const onError = (val: { title?: string }): void => {
-    if (val.title) {
-        setTitleErrors(val.title)
-    }
-}
-const update = async () => {
+const update = () => {
     form.title = titleValue.value
     form.icon = iconValue.value
-    router.post(`/admin/categories/${category.slug}`, {_method: 'patch', ...form}, {onError})
+    form.post(`/admin/categories/${category.slug}`);
+
 }
+
+const loadFile = ($event) => iconValue.value = $event.target.files[0]
+const clearFormErrors = () => form.clearErrors()
+
 </script>
 
 <template>
@@ -57,11 +57,12 @@ const update = async () => {
                 <div class="space-x-5">
                     <label for="title">Title</label>
                     <input id="title" v-model="titleValue"
-                           :class="{'border border-red-700': titleError }"
-                           class="rounded-md" name="title"
-                           type="text"/>
-                    <span v-if="titleError" class="text-sm text-red-500">
-                        {{ titleError }}
+                           :class="{'border border-red-700': titleError || form.errors.title }"
+                           class="rounded-md"
+                           name="title" type="text"
+                           @input="clearFormErrors"/>
+                    <span v-if="titleError || form.errors.title" class="text-sm text-red-500">
+                         {{ titleError }} {{ form.errors.title }}
                     </span>
                 </div>
 
@@ -74,19 +75,19 @@ const update = async () => {
                     <div class="border-r mx-4 border-gray-800 h-3 sm:block hidden"></div>
                     <div class="flex items-center space-x-5">
                         <label for="icon">New Icon</label>
-                        <input id="icon" :class="{'border border-red-700': iconError }"
+                        <input id="icon" :class="{'border border-red-700': iconError || form.errors.icon  }"
                                accept="image/png"
                                class="w-3/5 py-2 px-3 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
                                name="icon"
-                               type="file" @input="iconValue = $event.target.files[0]"/>
+                               type="file" @change="clearFormErrors" @input="loadFile"/>
                     </div>
 
-                    <span v-if="iconError" class="text-sm text-red-500">
-                        {{ iconError }}
+                    <span v-if="iconError || form.errors.icon" class="text-sm text-red-500">
+                        {{ iconError }} {{ form.errors.icon }}
                     </span>
                 </div>
 
-                <button class="pt-2.5 pb-1.5 px-4 bg-orange-400 text-white rounded-md"
+                <button :disabled="form.processing" class="pt-2.5 pb-1.5 px-4 bg-orange-400 text-white rounded-md"
                         type="submit">
                     Update
                 </button>
