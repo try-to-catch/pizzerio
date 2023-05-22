ede244bb4615b25cfa9e42dae50483baf42944ec
Kostya <koooooostaaa@gmail.com>
2023-05-22 16:07:43 +0300

    minor changes

--- resources/js/Pages/Admin/Categories/Create.vue
+++ resources/js/Pages/Admin/Categories/Create.vue
@@ -9,12 +9,12 @@ import {mixed, string} from 'yup'
 const {errorMessage: titleError, value: titleValue} = useField<string>('title', string().required().min(3))
 const {errorMessage: iconError, value: iconValue} = useField<null | File>('icon', mixed().required())
 
-interface NewCategory {
+interface INewCategory {
     title: string,
     icon: null | File
 }
 
-const form = useForm<NewCategory>({
+const form = useForm<INewCategory>({
     title: '',
     icon: null
 })
@@ -45,21 +45,22 @@ const store = async () => {
                            :class="{'border border-red-700': titleError || form.errors.title }"
                            class="rounded-md" name="title" required
                            type="text"/>
-                    <span v-if="titleError|| form.errors.title" class="text-sm text-red-500">{{
-                            titleError
-                        }} {{ form.errors.title }}</span>
+                    <span v-if="titleError || form.errors.title" class="text-sm text-red-500">
+                        {{ titleError }} {{ form.errors.title }}
+                    </span>
                 </div>
 
                 <div class="space-x-5">
                     <label for="icon">Icon</label>
                     <input id="icon" :class="{'border border-red-700': iconError || form.errors.icon }"
-                           accept="image/png" class="py-2 px-3 w-1/3 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
+                           accept="image/png"
+                           class="py-2 px-3 w-1/3 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
                            name="icon"
                            required
                            type="file" @input="iconValue = $event.target.files[0]"/>
-                    <span v-if="iconError || form.errors.icon" class="text-sm text-red-500">{{
-                            iconError
-                        }} {{ form.errors.icon }}</span>
+                    <span v-if="iconError || form.errors.icon" class="text-sm text-red-500">
+                        {{ iconError }} {{ form.errors.icon }}
+                    </span>
                 </div>
 
                 <button :disabled="form.processing" class="pt-2.5 pb-1.5 px-4 bg-primary text-white rounded-md"
