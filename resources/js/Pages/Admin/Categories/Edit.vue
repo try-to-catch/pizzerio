93f2d31f2f9d8aeb6ddaef977279520cb1e32ff8
Kostya <koooooostaaa@gmail.com>
2023-05-23 20:51:19 +0300

    refactor category edition form, improve code maintainability

--- resources/js/Pages/Admin/Categories/Edit.vue
+++ resources/js/Pages/Admin/Categories/Edit.vue
@@ -2,41 +2,48 @@
 
 import AdminLayout from "@/Layouts/AdminLayout.vue";
 import {Head, Link, useForm} from "@inertiajs/vue3";
-import {useField} from "vee-validate"
-import {string} from 'yup'
-import {ICategory} from "@/types/ICategory";
+import {object, string} from 'yup'
+import type {ICategory} from "@/types/ICategory";
+import type {ICategoryFormData} from "@/types/ICategoryFormData";
+import DefaultAdminForm from "@/Components/Form/DefaultAdminForm.vue";
+import InputField from "@/Components/Form/InputField.vue";
+import {computed} from "vue";
+import OrangeButton from "@/Components/Form/OrangeButton.vue";
+import FileInput from "@/Components/Form/FileInput.vue";
 
 const {category} = defineProps<{ category: ICategory }>()
 
-const {
-    errorMessage: titleError,
-    value: titleValue,
-} = useField<string>('title', string().min(3))
+const categorySchema = object({
+    title: string().min(3),
+})
 
-titleValue.value = category.title;
-
-const {errorMessage: iconError, value: iconValue} = useField<null | File>('icon')
-
-interface IUpdatedCategory {
-    title: string,
-    icon: null | File,
-    _method: 'put' | 'patch'
-}
-
-const form = useForm<IUpdatedCategory>({
-    title: '',
+const form = useForm<ICategoryFormData>({
+    title: category.title,
     icon: null,
     _method: 'patch'
 })
 
-const update = () => {
-    form.title = titleValue.value
-    form.icon = iconValue.value
-    form.post(`/admin/categories/${category.slug}`);
+const update = async () => {
+    const errors = await categorySchema.validate(form, {abortEarly: false}).catch((errors) => {
+        errors.inner.forEach(error => {
+            const splitError = error.message.split(' ')
+            splitError.shift()
 
+            const formattedMessage = `This ${splitError.join(' ')}`
+            form.setError(error.path, formattedMessage)
+        })
+    });
+
+    if (!errors) {
+        form.post(`/admin/categories/${category.slug}`)
+
+    }
 }
 
-const loadFile = ($event) => iconValue.value = $event.target.files[0]
+
+const isDisabled = computed((): boolean => Boolean(form.processing || form.errors.icon))
+
+const loadFile = ($event) => form.icon = $event.target.files[0]
 const clearFormErrors = () => form.clearErrors()
 
 </script>
@@ -44,7 +51,7 @@ const clearFormErrors = () => form.clearErrors()
 <template>
     <Head><title>Categories</title></Head>
     <admin-layout title="Categories - Edit">
-        <div class="xl:px-40 lg:px-28 md:px-16 sm:px-4 pt-10">
+        <div class="xl:w-[900px] lg:w-[720px] md:w-[640px] sm:w-auto sm:mx-5 mx-3 md:mx-auto pt-10">
             <div class="flex justify-end">
                 <Link :href="route('admin.categories.index')"
                       class="pt-2.5 pb-1.5 px-4 bg-gray-300 text-white rounded-t-md">
@@ -52,46 +59,14 @@ const clearFormErrors = () => form.clearErrors()
                 </Link>
             </div>
 
-            <form class="relative overflow-x-auto shadow-md sm:rounded-b-lg px-5 py-3 space-y-4 bg-white"
-                  @submit.prevent="update">
-                <div class="space-x-5">
-                    <label for="title">Title</label>
-                    <input id="title" v-model="titleValue"
-                           :class="{'border border-red-700': titleError || form.errors.title }"
-                           class="rounded-md"
-                           name="title" type="text"
-                           @input="clearFormErrors"/>
-                    <span v-if="titleError || form.errors.title" class="text-sm text-red-500">
-                         {{ titleError }} {{ form.errors.title }}
-                    </span>
-                </div>
+            <default-admin-form @submit.prevent="update">
+                <input-field id="title" v-model="form.title" :errors="form.errors.title" label="Title"
+                             @input="clearFormErrors"/>
 
-                <div class="sm:space-y-0 space-y-4 sm:flex sm:items-center">
-                    <div class="flex items-center space-x-5">
-                        <label for="icon">Current Icon</label>
-                        <img :alt="category.title" :src="category.icon" class="h-10 w-10"/>
-                    </div>
+                <file-input :errors="form.errors.icon" label="Icon" @input="loadFile"/>
 
-                    <div class="border-r mx-4 border-gray-800 h-3 sm:block hidden"></div>
-                    <div class="flex items-center space-x-5">
-                        <label for="icon">New Icon</label>
-                        <input id="icon" :class="{'border border-red-700': iconError || form.errors.icon  }"
-                               accept="image/png"
-                               class="w-3/5 py-2 px-3 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
-                               name="icon"
-                               type="file" @change="clearFormErrors" @input="loadFile"/>
-                    </div>
-
-                    <span v-if="iconError || form.errors.icon" class="text-sm text-red-500">
-                        {{ iconError }} {{ form.errors.icon }}
-                    </span>
-                </div>
-
-                <button :disabled="form.processing" class="pt-2.5 pb-1.5 px-4 bg-orange-400 text-white rounded-md"
-                        type="submit">
-                    Update
-                </button>
-            </form>
+                <orange-button :disabled="isDisabled">Update</orange-button>
+            </default-admin-form>
         </div>
     </admin-layout>
 </template>
