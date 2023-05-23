93c2686064603a2fc52d3b01e89054eb4ebe27f7
Kostya <koooooostaaa@gmail.com>
2023-05-23 20:53:28 +0300

    create products migration

---
+++ database/migrations/2023_05_20_122922_create_products_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+
+            $table->decimal('price', 6);
+            $table->decimal('sale_price', 6)->nullable();
+
+            $table->smallInteger('priority')->default(1);
+
+            $table->string('thumbnail');
+            $table->string('ingredients')->nullable();
+            $table->text('description')->nullable();
+
+            $table->boolean('is_for_sale')->default(false);
+
+            $table->string('banner')->default(false);
+
+            $table->foreignIdFor(Category::class)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (!app()->isProduction()) {
+            Schema::dropIfExists('products');
+        }
+    }
+};
