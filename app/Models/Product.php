be299ee4f2efc516a815de6fc13d911a0413af46
Kostya <koooooostaaa@gmail.com>
2023-05-23 20:56:03 +0300

    create product model

---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'price',
+        'sale_price',
+        'priority',
+        'thumbnail',
+        'ingredients',
+        'description',
+        'banner',
+        'category_id',
+        'is_for_sale'];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function (Product $product) {
+            $product->slug = $product->slug ?? str($product->title)->slug();
+        });
+
+    }
+}
