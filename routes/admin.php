a38fd725050161c94c96cd1602844165c37def97
Kostya <koooooostaaa@gmail.com>
2023-05-23 20:43:27 +0300

    add new resource routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -10,14 +11,5 @@ Route::get('/', function () {
     return Inertia::render('Admin/Dashboard');
 })->name('dashboard');
 
-Route::get('/products', function () {
-    return Inertia::render('Admin/Dashboard');
-})->name('products');
-
-Route::bind('category', function ($value) {
-    return Category::where('slug', $value)->firstOrFail();
-});
-
 Route::resource('categories', CategoryController::class);
-
-require __DIR__ . '/auth.php';
+Route::resource('products', ProductController::class);
