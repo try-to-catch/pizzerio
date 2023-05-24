4247ad45ca8f4ef68e3b4a64677c5b1727873305
Kostya <koooooostaaa@gmail.com>
2023-05-24 20:19:07 +0300

    cosmetic changes

--- resources/js/Components/Form/OrangeButton.vue
+++ resources/js/Components/Form/OrangeButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="pt-2.5 pb-1.5 px-4 text-white rounded-md w-full bg-orange-400" :type="type">
+    <button class="pt-2.5 pb-1.5 px-4 text-white rounded-md w-full bg-orange-400 enabled:hover:bg-orange-500 ease-in-out duration-300 disabled:opacity-75" :type="type">
         <slot/>
     </button>
 </template>
