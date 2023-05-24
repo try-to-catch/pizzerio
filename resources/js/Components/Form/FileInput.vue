0b455ae85d9039c224814a21e812295a1ffe9d6a
Kostya <koooooostaaa@gmail.com>
2023-05-24 20:13:40 +0300

    update component add '*' when field is required

--- resources/js/Components/Form/FileInput.vue
+++ resources/js/Components/Form/FileInput.vue
@@ -8,15 +8,20 @@ withDefaults(defineProps<{
 }>(), {id: 'icon', label: 'Icon', required: false, accept: 'image/png'})
 </script>
 <template>
-    <div class="flex flex-col ">
-        <label :for="id">{{ label }}</label>
+    <div>
+        <div class="flex items-center justify-between">
+            <label :for="id" class="capitalize relative block mb-0.5 text-sm font-medium text-gray-900">
+                {{ label }}
+                <span v-if="required" class="text-red-600 absolute -top-1 -right-2.5">*</span>
+            </label>
+            <span v-if="errors" :class="$style['capitalize-first']" class="text-sm text-red-500">{{ errors }}</span>
+        </div>
         <input :id="id" :accept="accept"
                :class="{'border border-red-700': errors}"
                :name="id"
                :required="required" accept="image/png,jpg"
-               class="py-2 px-3 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
+               class="w-full py-2 px-3 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
                type="file"/>
-        <span v-if="errors" :class="[$style['capitalize-first']]" class="text-sm text-red-500">{{ errors }}</span>
     </div>
 </template>
 
--- resources/js/Components/Form/InputField.vue
+++ resources/js/Components/Form/InputField.vue
@@ -6,7 +6,7 @@ interface InputFieldProps {
     errors?: string,
     type?: string,
     required?: boolean,
-    modelValue?: string
+    modelValue?: number | string
 }
 
 withDefaults(defineProps<InputFieldProps>(), {type: 'text', required: false})
@@ -18,7 +18,10 @@ defineEmits<{ 'update:ModelValue': (value: string) => void }>()
 <template>
     <div>
         <div class="flex items-center justify-between">
-            <label :for="id" class="capitalize">{{ label }}</label>
+            <label :for="id" class="capitalize relative block mb-0.5 text-sm font-medium text-gray-900">
+                {{ label }}
+                <span v-if="required" class="text-red-600 absolute -top-1 -right-2.5">*</span>
+            </label>
             <span v-if="errors" :class="$style['capitalize-first']" class="text-sm text-red-500">{{ errors }}</span>
         </div>
         <input :id="id" :class="{'border border-red-700': errors }"
