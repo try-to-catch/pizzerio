6841079d3c4741dbbc3b345c275b740f1aa4479c
Kostya <koooooostaaa@gmail.com>
2023-05-24 20:18:25 +0300

    update banner length limit

--- resources/js/composables/admin/product.ts
+++ resources/js/composables/admin/product.ts
@@ -3,7 +3,7 @@ import {boolean, number, object, string} from "yup";
 export default function useProduct() {
     const productSchema = object({
         title: string().min(3),
-        banner: string().min(2).nullable(),
+        banner: string().min(2).max(5).nullable(),
         categoryId: number().positive(),
         description: string().min(50),
         ingredients: string().min(20),
