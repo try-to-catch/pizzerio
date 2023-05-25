06484ad6cf8c1d74e7a59da35894d27c8aac2902
Kostya <koooooostaaa@gmail.com>
2023-05-25 22:24:30 +0300

    add new relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Category::class);
     }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
