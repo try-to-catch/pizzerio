13ae58c2ab17820f756c88629acf72e0197b0bfc
Kostya <koooooostaaa@gmail.com>
2023-05-25 22:25:32 +0300

    minor changes

--- resources/js/Components/Form/TextArea.vue
+++ resources/js/Components/Form/TextArea.vue
@@ -9,7 +9,7 @@ withDefaults(defineProps<{
 }>(), {id: 'icon', label: 'Icon', required: false, rows: '3'})
 </script>
 <template>
-    <div class="">
+    <div>
         <div class="flex items-center justify-between">
             <label :for="id" class="capitalize relative block mb-0.5 text-sm font-medium text-gray-900">
                 {{ label }}
@@ -17,8 +17,8 @@ withDefaults(defineProps<{
             </label>
             <span v-if="errors" :class="$style['capitalize-first']" class="text-sm text-red-500">{{ errors }}</span>
         </div>
-        <textarea :id="id" :required="required" :rows="rows"
-                  :value="modelValue"
+        <textarea :id="id" :class="{'border border-red-700': errors }" :required="required"
+                  :rows="rows" :value="modelValue"
                   class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                   @input="$emit('update:modelValue', $event.target.value)"></textarea>
     </div>
