9c28c140e4e54a316e5a9529863c253c4ae960aa
Kostya <koooooostaaa@gmail.com>
2023-05-25 22:28:53 +0300

    create new test

---
+++ tests/Feature/Admin/ProductsTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class ProductsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Storage::fake();
+
+        $this->withHeaders([
+            'accept' => 'application/json'
+        ]);
+    }
+
+    public function test_admin_can_see_index_page(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->get('/admin/products');
+
+        $response->assertOk();
+    }
+
+    public function test_admin_can_see_create_page(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->get('/admin/products/create');
+
+        $response->assertOk();
+    }
+
+    public function test_admin_can_create_category(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        Category::factory()->count(1)->create(['user_id' => $user->id])->first();
+
+        $thumbnail = UploadedFile::fake()->image('avatar.jpg', 500, 500);
+
+        $data = [
+            'title' => 'Pizza Pepperoni',
+            'banner' => 'New',
+            'category_id' => 1,
+            'description' => 'Delicious pizza for the whole family, buy and rejoice, bc our pizza is the best',
+            'ingredients' => '1/2 cup pizza sauce (see below) 18 to 20 slices pepperoni',
+            'is_for_sale' => true,
+            'price' => 1.23,
+            'sale_price' => null,
+            'priority' => 5,
+            'thumbnail' => $thumbnail
+        ];
+
+        $response = $this->post('/admin/products', $data);
+        $response->assertRedirect();
+
+        $this->assertDatabaseCount('products', 1);
+
+        $product = Product::first();
+        $data['thumbnail'] = 'images/products/' . $thumbnail->hashName();
+
+        $this->assertEquals($data, [
+            'title' => $product->title,
+            'banner' => $product->banner,
+            'category_id' => $product->category_id,
+            'description' => $product->description,
+            'ingredients' => $product->ingredients,
+            'is_for_sale' => $product->is_for_sale,
+            'price' => $product->price,
+            'sale_price' => $product->sale_price,
+            'priority' => $product->priority,
+            'thumbnail' => $product->thumbnail,
+
+        ]);
+
+        Storage::disk('public')->assertExists($product->url);
+    }
+}
