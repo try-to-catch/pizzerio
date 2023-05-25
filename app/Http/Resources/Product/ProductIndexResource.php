01a98b9fa932205300fc294ff335fcf8d114577f
Kostya <koooooostaaa@gmail.com>
2023-05-25 22:30:21 +0300

    create product index resource

---
+++ app/Http/Resources/Product/ProductIndexResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductIndexResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'slug' => $this->slug,
+            'title' => $this->title,
+            'price' => $this->price,
+            'thumbnail' => '/storage/' . $this->thumbnail,
+            'updated_at' => $this->updated_at->diffForHumans(),
+            'created_by' => $this->user_email,
+            'category_title' => $this->category_title,
+            'category_slug' => $this->category_slug
+        ];
+    }
+}
