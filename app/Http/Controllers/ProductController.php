3601a335543eac40d32c1d231f4c365992ec34ab
Kostya <koooooostaaa@gmail.com>
2023-05-28 21:31:29 +0300

    improve index method with search functionality and pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,23 +8,38 @@ use App\Http\Resources\Category\CategoryMinResource;
 use App\Http\Resources\Product\ProductIndexResource;
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 use Intervention\Image\Facades\Image;
 
 class ProductController extends Controller
 {
-    public function index(): Response
+    public function index(Request $request): Response
     {
 
+        $searchQuery = $request->only('s');
+        $parsedSearch = array_shift($searchQuery);
+
+        if (gettype($parsedSearch) !== 'string') {
+            abort(400);
+        }
+
         $products = Product::query()
-            ->select('products.id', 'products.slug', 'products.title', 'products.price', 'products.thumbnail', 'products.updated_at', 'users.email as user_email', 'categories.title as category_title', 'categories.slug as category_slug')
+            ->select('products.id', 'products.slug', 'products.title', 'products.price', 'products.is_for_sale', 'products.thumbnail', 'products.updated_at', 'users.email as user_email', 'categories.title as category_title', 'categories.slug as category_slug')
             ->join('users', 'users.id', '=', 'products.user_id')
             ->join('categories', 'categories.id', '=', 'products.category_id')
-            ->get();
+            ->when($parsedSearch, function (Builder $query) use ($parsedSearch) {
+                $query->where('products.title', 'like', '%' . $parsedSearch . '%');
+            })
+            ->orderByDesc('products.updated_at')
+            ->paginate(6);
 
-        $dataForResponse['products'] = ProductIndexResource::collection($products)->resolve();
+        $dataForResponse['products'] = $products->toArray();
+
+        $dataForResponse['products']['data'] = ProductIndexResource::collection($products)->resolve();
 
         $message = session('message');
         if ($message) {
