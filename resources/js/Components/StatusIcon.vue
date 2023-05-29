da08ec9ff02e11ad0c4a850edc9aeeb1da01436c
Kostya <koooooostaaa@gmail.com>
2023-05-29 19:58:09 +0300

    create new component

---
+++ resources/js/Components/StatusIcon.vue
@@ -0,0 +1,13 @@
+<template>
+    <font-awesome-icon v-if="isSuccess" :icon="faCircleCheck" class="text-green-500"
+                       size="lg"/>
+    <font-awesome-icon v-else :icon="faCircleXmark" class="text-red-500"
+                       size="lg"/>
+</template>
+
+<script lang="ts" setup>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faCircleCheck, faCircleXmark} from "@fortawesome/free-solid-svg-icons";
+
+withDefaults(defineProps<{ isSuccess: boolean, size: string }>(), {size: 'lg'})
+</script>
