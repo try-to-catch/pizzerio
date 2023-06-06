81696d1f44a2596855b00d0daada96c08fbb27b7
Kostya <koooooostaaa@gmail.com>
2023-06-06 19:08:25 +0300

    add a new required property

--- resources/js/types/ICategory.ts
+++ resources/js/types/ICategory.ts
@@ -3,6 +3,7 @@ export interface ICategory {
     slug: string;
     title: string;
     icon: string;
+    number_of_related_products: number;
     created_by: string;
     created_at: string;
     updated_at: string;
