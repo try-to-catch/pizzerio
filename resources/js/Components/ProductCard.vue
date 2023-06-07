b578ba618bb6289c4ed0d9217b729d87b930d17f
Kostya <koooooostaaa@gmail.com>
2023-06-07 21:02:32 +0300

    update components, make dynamic

--- resources/js/Components/ProductCard.vue
+++ resources/js/Components/ProductCard.vue
@@ -1,22 +1,26 @@
 <script lang="ts" setup>
 
-import type {IProductCard} from "@/types/IProductCard";
+import type {IProductCardEssentials} from "@/types/IProductCardEssentials";
 
-const {product} = defineProps<{product: IProductCard}>()
+const {product} = defineProps<{ product: IProductCardEssentials }>()
 </script>
 
 <template>
     <div class="rounded-xl bg-white relative border border-gray-100">
-        <div v-if="product.status" class="absolute py-[7px] px-[18px] text-sm bg-red-bg text-white uppercase left-0 top-5 rounded-r-md">{{ product.status }}</div>
+        <div v-if="product.banner"
+             class="absolute py-[7px] px-[18px] text-sm bg-red-bg text-white uppercase left-0 top-5 rounded-r-md">
+            {{ product.banner }}
+        </div>
         <div class="max:h-300 max:w-300 overflow-hidden flex items-center justify-center">
             <img :alt="product.title" :src="product.thumbnail">
         </div>
         <div class="m-5 mt-4">
-            <h2 class="font-semibold text-lg">{{ product.title }}</h2>
-            <p class="mt-3  line-clamp-2">{{  product.description }}</p>
+            <h2 class="font-semibold capitalize">{{ product.title }}</h2>
+            <p class="mt-3  line-clamp-2">{{ product.ingredients }}</p>
             <div class="flex justify-between items-center mt-4">
                 <button class="text-white bg-primary py-[13px] px-8 rounded-md">Выбрать</button>
-                <span class="text-primary font-semibold text-lg">от {{ product.price }}$</span>
+                <span v-if="!product.sale_price" class="text-primary font-semibold text-lg">от {{ product.price }}$</span>
+                <span v-else class="text-red font-semibold text-lg ">от {{ product.sale_price }}$</span>
             </div>
         </div>
     </div>
--- resources/js/Components/ProductsGrid.vue
+++ resources/js/Components/ProductsGrid.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup>
-
+import {Link} from "@inertiajs/vue3";
 import ProductCard from "@/Components/ProductCard.vue";
 import FilterIcon from "@/Components/Icons/FilterIcon.vue";
+import {ICategoryEssentialsWithProductCards} from "@/types/ICategoryEssentialsWithProductCards";
+
+const {category} = defineProps<{ category: ICategoryEssentialsWithProductCards }>()
 </script>
 
 <template>
     <div>
         <div class="flex justify-between mb-8 items-center">
-            <h3 class="text-[40px] leading-[48px] font-semibold">Пицца</h3>
+            <h3 class="text-[40px] leading-[48px] font-semibold"><Link :href="route('products')">{{category.title}}</Link></h3>
             <div class="py-[9px] px-4 bg-white flex items-center rounded-md border border-gray-100" role="button">
                 <filter-icon class="mr-2 h-4 w-4"/>
                 Фильтры
@@ -15,13 +18,7 @@ import FilterIcon from "@/Components/Icons/FilterIcon.vue";
         </div>
 
         <div class="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[30px]">
-            <ProductCard v-for="i in 9" :product="{
-                    id: 1,
-                    title: 'Чикен Сладкий Чили',
-                    description: 'Курица, Лук, Перец Халапеньо, Сыр Моцарелла, Томатный соус, Паприка, Майонез',
-                    price: 4.5,
-                    thumbnail: '/images/nice-pizza.png',
-                    status: 'Hot'}"/>
+            <ProductCard v-for="product in category.products" :product="product"/>
         </div>
     </div>
 </template>
