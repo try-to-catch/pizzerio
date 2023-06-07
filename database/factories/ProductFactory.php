7bfe096512137dfd2566f61c919bb973fd7cb64c
Kostya <koooooostaaa@gmail.com>
2023-06-07 21:04:14 +0300

    add real thumbnail to schema

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,29 +2,33 @@
 
 namespace Database\Factories;
 
+use App\Actions\Product\StoreThumbnailAction;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\File;
 
 /**
  * @extends Factory<Product>
  */
 class ProductFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $thumbnailPath = storage_path('app/public/images/defaults/nice-pizza.png');
+        $thumbnail = new File($thumbnailPath);
+
+        $storeThumbnailAction = new StoreThumbnailAction();
+        $storagePath = $storeThumbnailAction->handle($thumbnail);
+
         return [
             'title' => fake()->unique()->word(),
-            'price' => fake()->numberBetween(10,100),
-            'priority' => fake()->numberBetween(1,999),
-            'thumbnail' => '',
+            'price' => fake()->numberBetween(10, 100),
+            'priority' => fake()->numberBetween(1, 999),
+            'thumbnail' => $storagePath,
             'ingredients' => fake()->words(8, true),
             'description' => fake()->paragraph(),
             'is_for_sale' => true,
+            'banner' => null,
         ];
     }
 }
