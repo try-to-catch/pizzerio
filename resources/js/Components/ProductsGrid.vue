85bd5073748e0f8d0e19272e54227b82fc89f6d5
Kostya <koooooostaaa@gmail.com>
2023-06-11 20:50:31 +0300

    add onClick event emitting

--- resources/js/Components/ProductsGrid.vue
+++ resources/js/Components/ProductsGrid.vue
@@ -5,12 +5,23 @@ import FilterIcon from "@/Components/Icons/FilterIcon.vue";
 import {ICategoryEssentialsWithProductCards} from "@/types/ICategoryEssentialsWithProductCards";
 
 const {category} = defineProps<{ category: ICategoryEssentialsWithProductCards }>()
+
+
+const emit = defineEmits<{ (e: 'productChosen', value: string): void }>()
+
+const notifyBaseComponent = (productId: string) => {
+    emit('productChosen', productId)
+}
 </script>
 
 <template>
     <div>
         <div class="flex justify-between mb-8 items-center">
-            <h3 class="text-[40px] leading-[48px] font-semibold"><Link :href="route('products')">{{category.title}}</Link></h3>
+            <h3 class="text-[40px] leading-[48px] font-semibold">
+                <Link :href="route('products')">
+                    {{ category.title }}
+                </Link>
+            </h3>
             <div class="py-[9px] px-4 bg-white flex items-center rounded-md border border-gray-100" role="button">
                 <filter-icon class="mr-2 h-4 w-4"/>
                 Фильтры
@@ -18,7 +29,8 @@ const {category} = defineProps<{ category: ICategoryEssentialsWithProductCards }
         </div>
 
         <div class="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[30px]">
-            <ProductCard v-for="product in category.products" :product="product"/>
+            <ProductCard v-for="product in category.products" :key="product.id"
+                         :product="product" @click="notifyBaseComponent(product.slug)"/>
         </div>
     </div>
 </template>
