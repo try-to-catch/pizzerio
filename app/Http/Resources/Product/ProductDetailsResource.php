ff2bc73b3aed41d80c289ef6f1a45f3394e84a8d
Kostya <koooooostaaa@gmail.com>
2023-06-11 20:45:57 +0300

    create ProductDetails resource

---
+++ app/Http/Resources/Product/ProductDetailsResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductDetailsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return[
+            'id' => $this->id,
+            'slug' => $this->slug,
+            'title' => $this->title,
+            'ingredients' => $this->ingredients,
+            'description' => $this->ingredients,
+            'price' => $this->price,
+            'sale_price' => $this->sale_price,
+            'banner' => $this->banner,
+            'thumbnail' => '/storage/' . $this->thumbnail,
+        ];
+    }
+}
