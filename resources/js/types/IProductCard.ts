5758f4d4c1b605db5e5c524c1980bd51138228ff
Kostya <koooooostaaa@gmail.com>
2023-06-11 20:47:13 +0300

    remove unnecessary interface

--- resources/js/types/IProductCard.ts
+++
@@ -1,7 +0,0 @@
-import {IProductCardEssentials} from "@/types/IProductCardEssentials";
-
-export interface IProductCard extends IProductCardEssentials {
-    readonly sale_price: number | null;
-    readonly priority: number;
-    readonly banner: string | null;
-}
