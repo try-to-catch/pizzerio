1e7e103d61b387d626e0ddcb525823f9202ea9cd
Kostya <koooooostaaa@gmail.com>
2023-06-15 22:07:28 +0300

    remove enums

--- resources/js/types/IOrderEssentials.ts
+++ resources/js/types/IOrderEssentials.ts
@@ -2,17 +2,5 @@ export interface IOrderEssentials {
     slug: string;
     title: string;
     thumbnail: string;
-    dough_type: DoughTypeEnum;
-    diameter: PizzaDiameterEnum;
 }
 
-export enum DoughTypeEnum {
-    Traditional = 'Традиционное',
-    Thin = 'Тонкое'
-}
-
-export enum PizzaDiameterEnum {
-    Small = '20',
-    Medium = '28',
-    Large = '33'
-}
