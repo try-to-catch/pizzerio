b1badbdada8508a48a98db01f4a567d9b08a5fd3
Kostya <koooooostaaa@gmail.com>
2023-06-20 20:41:55 +0300

    improve responsiveness for smaller screens to enhance adaptability

--- resources/js/Components/ProductCard.vue
+++ resources/js/Components/ProductCard.vue
@@ -6,21 +6,25 @@ const {product} = defineProps<{ product: IProductCardEssentials }>()
 </script>
 
 <template>
-    <div class="rounded-xl bg-white relative border border-gray-100 cursor-pointer">
+    <div class="rounded-xl bg-white relative border border-gray-100 cursor-pointer flex flex-row sm:flex-col h-[124px] sm:h-auto sm:max-h-full p-3 sm:p-0">
         <div v-if="product.banner"
-             class="absolute py-[7px] px-[18px] text-sm bg-red-bg text-white uppercase left-0 top-5 rounded-r-md">
+             class="absolute sm:py-[7px] sm:px-[18px] px-2.5 py-1 sm:text-sm bg-red-bg text-white uppercase left-0 sm:top-5 top-2 rounded-r-md text-xs">
             {{ product.banner }}
         </div>
-        <div class="overflow-hidden flex items-center justify-center">
-            <img class="max-h-[300px] max-w-full bg-cover " :alt="product.title" :src="product.thumbnail">
+        <div class="overflow-hidden flex items-center justify-center sm:w-auto  w-[100px] shrink-0">
+            <img class="sm:max-h-[300px] w-auto bg-cover" :alt="product.title" :src="product.thumbnail">
         </div>
-        <div class="m-5 mt-4">
-            <h2 class="font-semibold capitalize">{{ product.title }}</h2>
-            <p class="mt-3  line-clamp-2">{{ product.ingredients }}</p>
-            <div class="flex justify-between items-center mt-4">
-                <button class="text-white bg-primary py-[13px] px-8 rounded-md">Выбрать</button>
+        <div class="sm:m-5 sm:mt-4 ml-3 flex flex-col sm:justify-between justify-center grow">
+            <h2 class="font-semibold capitalize text-sm sm:text-lg">{{ product.title }}</h2>
+            <p class="sm:mt-3 mt-2 line-clamp-2 text-xs sm:text-base pb-2.5 sm:pb-0">{{ product.ingredients }}</p>
+            <div class="sm:flex hidden justify-between items-center mt-4">
+                <button class="text-white bg-primary py-[13px] px-8 rounded-md hover:bg-orange-600 ease-in-out duration-300">Выбрать</button>
                 <span v-if="!product.sale_price" class="text-primary font-semibold text-lg">от {{ product.price }}$</span>
                 <span v-else class="text-red-500 font-semibold text-lg ">от {{ product.sale_price }}$</span>
+            </div>
+            <div class="flex sm:hidden items-center text-sm">
+                <div class="py-[7px] px-4 rounded-md bg-light-primary text-primary font-semibold">от {{ product.price }}$</div>
+                <div v-if="product.sale_price" class="ml-3 text-gray-400 line-through">от {{ product.sale_price }}$</div>
             </div>
         </div>
     </div>
--- resources/js/Components/ProductsGrid.vue
+++ resources/js/Components/ProductsGrid.vue
@@ -17,19 +17,19 @@ const notifyBaseComponent = (product: IProductCardEssentials) => {
 
 <template>
     <div>
-        <div class="flex justify-between mb-8 items-center">
-            <h3 class="text-[40px] leading-[48px] font-semibold">
+        <div class="flex justify-between sm:mb-8 mb-5 items-center">
+            <h3 class="sm:text-[40px] sm:leading-[48px] text-[28px] leading-[32px] font-semibold">
                 <Link :href="route('products')">
                     {{ category.title }}
                 </Link>
             </h3>
-            <div class="py-[9px] px-4 bg-white flex items-center rounded-md border border-gray-100" role="button">
+            <div class="py-[9px] px-4 bg-white flex items-center rounded-md border border-gray-100 text-sm sm:text-base" role="button">
                 <filter-icon class="mr-2 h-4 w-4"/>
                 Фильтры
             </div>
         </div>
 
-        <div class="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[30px]">
+        <div class="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-[30px] gap-3">
             <ProductCard v-for="product in category.products" :key="product.id"
                          :product="product" @click="notifyBaseComponent(product)"/>
         </div>
