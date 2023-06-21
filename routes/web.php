38501dad5051b1f33a1f89b33432588042da473a
Kostya <koooooostaaa@gmail.com>
2023-06-21 21:23:06 +0300

    create a new route

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,15 @@
 <?php
 
+use App\Http\Controllers\Category\ShowController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', HomeController::class)->name('home');
 
-Route::get('/pizza', function () {
-    return Inertia::render('Products/Index');
-})->name('products');
-
-Route::get('/admin', function () {
-    return Inertia::render('Admin/Dashboard');
-})->name('products');
+Route::get('/categories/{category:slug}', ShowController::class)->name('categories.show');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
