e50580030bf4fe54df578085d8c411aaf7f9d1e8
Kostya <koooooostaaa@gmail.com>
2023-06-25 22:16:48 +0300

    enhance CartController by adding update and remove functionality for cart items

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Cart\AddToCartRequest;
+use App\Http\Requests\Cart\UpdateCartRequest;
 use App\Http\Resources\Cart\CartIndexResource;
 use App\Models\Cart;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -16,11 +18,11 @@ class CartController extends Controller
         $cart = Cart::query()->firstOrCreate(['session_id' => session()->getId()]);
 
         $products = $cart->products()
-            ->select(['id', 'slug', 'thumbnail', 'title', 'price', 'sale_price'])
+            ->select(['products.id', 'slug', 'thumbnail', 'title', 'price', 'sale_price'])
             ->withPivot('quantity')
             ->get();
 
-        return Inertia::render('Cart/Index', [
+        return Inertia::render('Cart', [
             'products' => CartIndexResource::collection($products)->resolve()
         ]);
     }
@@ -44,5 +46,27 @@ class CartController extends Controller
         $previousPath = preg_replace('/\?.*/', '', $previousPath);
 
         return redirect()->to($previousPath);
+    }
+
+    //update product quantity in cart
+    public function update($productId, UpdateCartRequest $request): JsonResponse
+    {
+        $quantity = $request->validated()['quantity'];
+
+        $cart = Cart::query()->firstOrCreate(['session_id' => session()->getId()]);
+
+        $cart->products()->updateExistingPivot($productId, ['quantity' => $quantity]);
+
+        return response()->json(['message' => 'Cart updated']);
+    }
+
+    //remove product from cart
+    public function remove($productId): JsonResponse
+    {
+        $cart = Cart::query()->firstOrCreate(['session_id' => session()->getId()]);
+
+        $cart->products()->detach($productId);
+
+        return response()->json(['message' => 'Product removed from cart']);
     }
 }
