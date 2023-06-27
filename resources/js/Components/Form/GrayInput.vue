e3638a2ea82ed714c4d42daf0dfcc0703b138efa
Kostya <koooooostaaa@gmail.com>
2023-06-27 21:33:55 +0300

    create new components

---
+++ resources/js/Components/Form/GrayInput.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+withDefaults(defineProps<{
+    required?: boolean,
+    type?: string,
+    placeholder?: string,
+    label?: string,
+    modelValue?: string
+}>(), {
+    required: false,
+    type: 'text',
+    label: '',
+})
+
+
+defineEmits<{ 'update:ModelValue': (value: string) => void }>()
+</script>
+
+<template>
+    <label class="text-sm font-medium text-gray-text sm:text-xs">
+        {{ label }}{{ required ? '*' : '' }}
+        <input :placeholder="placeholder" :required="required"
+               :type="type"
+               :value="modelValue"
+               class="mt-2 border border-gray-border text-gray-black-text text-sm rounded-lg focus:ring-transparent focus:border-orange-500 block w-full py-[13px] px-4 placeholder:text-gray-text" @input="$emit('update:modelValue', $event.target.value)">
+    </label>
+</template>
+
---
+++ resources/js/Components/Form/GrayTextArea.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+withDefaults(defineProps<{
+    required?: boolean,
+    errors?: string,
+    id?: string,
+    rows?: string,
+    label?: string,
+    modelValue?: string,
+    placeholder?: string,
+}>(), {id: 'icon', label: '', required: false, rows: '3'})
+</script>
+
+<template>
+    <div class="w-full">
+        <div class="flex items-center justify-between">
+            <label :for="id" class="capitalize relative block mb-0.5 text-sm font-medium text-gray-900">
+                {{ label }}{{ required ? '*' : '' }}
+            </label>
+            <span v-if="errors" :class="$style['capitalize-first']" class="text-sm text-red-500">{{ errors }}</span>
+        </div>
+        <textarea :id="id" :class="{'border border-red-700': errors }" :required="required"
+                  :rows="rows" :value="modelValue" :placeholder="placeholder"
+                  class="block px-4 pt-[13px] w-full sm:text-base text-sm text-black-text rounded-md border border-gray-border focus:ring-transparent focus:border-primary resize-none"
+                  @input="$emit('update:modelValue', $event.target.value)"></textarea>
+    </div>
+</template>
+
+<style module>
+.capitalize-first {
+    text-transform: lowercase;
+}
+
+.capitalize-first:first-letter {
+    text-transform: uppercase;
+}
+</style>
